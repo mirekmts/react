db340fa6138d6a28fd4d307b60c59cf399bb875d
Miroslaw Stasiak <mirekmts@gmail.com>
2017-07-18 23:35:36 +0200

    add a add and delete funcionality

--- reminder-pro/src/components/App.jsx
+++ reminder-pro/src/components/App.jsx
@@ -1,6 +1,46 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { addReminder, deleteReminder } from '../actions';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      text: ''
+    };
+  }
+
+  addReminder() {
+    this.props.addReminder(this.state.text)
+  }
+
+  deleteReminder(id) {
+    console.log('deleting in aplication', id);
+    console.log('this.props', this.props)
+  }
+
+  renderReminders() {
+    const { reminders } = this.props;
+    return (
+        <ul className="list-group col-sm-4">
+            {
+                reminders.map(reminder => {
+                    return (
+                        <li key={reminder.id} className="list-group-item">
+                            <div className="list-item">{reminder.text}</div>
+                            <div className="list-item delete-button"
+                                onClick={() => this.deleteReminder(reminder.id)}>
+                                &#x2715;
+                            </div>
+                        </li>
+                    )
+                })
+            }
+        </ul>
+    )
+  }
+
   render(){
     return(
       <div className="app">
@@ -11,15 +51,26 @@ class App extends Component {
           <div className="form-group">
             <input className="form-contorl"
               placeholder="I have to..."
+              onChange={event => this.setState({text: event.target.value})}
             />
-            <button type="button" className="btn btn-success">
-              Add Reminder
-            </button>
           </div>
+          <button
+            type="button"
+            className="btn btn-success"
+            onClick={() => this.addReminder()}>
+            Add Reminder
+          </button>
         </div>
+        { this.renderReminders() }
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    reminders: state
+  }
+}
+
+export default connect(mapStateToProps, {addReminder, deleteReminder})(App);
